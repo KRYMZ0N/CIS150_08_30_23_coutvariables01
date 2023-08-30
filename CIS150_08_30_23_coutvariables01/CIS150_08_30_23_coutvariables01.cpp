aa320d9768c95e00a89930d1699212a74cea13dd
KRYMZ0N <imkrymz0n@gmail.com>
2023-08-30 11:40:06 -0400

    add more

--- CIS150_08_30_23_coutvariables01/CIS150_08_30_23_coutvariables01.cpp
+++ CIS150_08_30_23_coutvariables01/CIS150_08_30_23_coutvariables01.cpp
@@ -3,16 +3,27 @@
 //Date: 08-30-23
 //Description: cout variables/chap2.
 //==========================================
-
+//
 #include <iostream>
 
 using namespace std;
 int main() {
+    string firsttri = "* \n**\n***\n\n";
+    string secondtri = "  *\n * *\n* * *\n\n";
+    string thirdtri = "  *\n **\n***";
     cout << "Hello this is a test!\n";
-    cout << "This is another test";
+    cout << "This is another test" << endl;
     
     cout << "Hello this is a test!" << endl;
-    cout << "This is another test";
+    cout << "This is another test" << endl;
     // Will display on different lines due to the \n
+
+    //==========================================
+    //Multiple string literals in one cout statement
+    //==========================================
+    cout << "Hello " << "my name is " << "Jack!" << endl;
+
+
+    cout << firsttri << secondtri << thirdtri;
     return 0;
 }
