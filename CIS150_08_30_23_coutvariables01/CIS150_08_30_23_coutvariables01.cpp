f8494475c0e7113fc00301d1bb6d54e544ef41e6
jgrimes34528 <jgrimes34528@DT-015503>
2023-08-30 11:11:04 -0400

    Add project files.

---
+++ CIS150_08_30_23_coutvariables01/CIS150_08_30_23_coutvariables01.cpp
@@ -0,0 +1,18 @@
+//==========================================
+//Author: Jack Grimes
+//Date: 08-30-23
+//Description: cout variables/chap2.
+//==========================================
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello this is a test!\n";
+    cout << "This is another test";
+    
+    cout << "Hello this is a test!" << endl;
+    cout << "This is another test";
+    // Will display on different lines due to the \n
+    return 0;
+}
